9d129bf49675203a3ee34da327c47dc29ece2ff6
DightMerc <dightmerc@gmail.com>
2020-02-23 21:58:32 +0500

    feat: updated

--- src/index.js
+++ src/index.js
@@ -1,12 +1,34 @@
-
 exports.min = function min (array) {
-  return 0;
+  if (array === undefined || array.length === 0) {
+    return 0;
+  }
+    if (array.length > 0) {
+      return Math.min(...array); 
+    } else {
+      return 0;
+    }
 }
 
 exports.max = function max (array) {
-  return 0;
+  if (array === undefined || array.length === 0) {
+    return 0;
+  }
+    if (array.length > 0) {
+      return Math.max(...array); 
+    } else {
+      return 0;
+    }
 }
 
 exports.avg = function avg (array) {
-  return 0;
+  if (array === undefined || array.length === 0) {
+    return 0;
+  }
+  let sum = 0;
+  for (let i = 0; i < array.length; i++) {
+    sum += parseInt(array[i], 10);
+  }
+  let avg = sum / array.length;
+  
+  return avg;
 }
